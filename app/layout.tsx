9e01d9822b77066ca12dfb38a149204ea5a9c7d1
hammadmajid <hammadmajid@proton.me>
2024-10-10 18:30:41 +0500

    include Header and Footer in root layout & format code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,45 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
+import {ThemeProvider} from "@/components/theme-provider"
+import {Header} from "@/components/Header";
+import {Footer} from "@/components/Footer";
+import React from "react";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "baserency",
-  description: "Compare one base currency with multiple others",
+    title: "baserency",
+    description: "Compare one base currency with multiple others",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          {children}
-      </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            <div className="flex justify-between flex-col min-h-screen">
+                <Header/>
+                {children}
+                <Footer/>
+            </div>
+        </ThemeProvider>
+        </body>
+        </html>
+    );
 }
